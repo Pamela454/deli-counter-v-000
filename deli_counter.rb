24c5de0a1aa1f18d5520a702f673149b5fc82432
Pamela Frieze <pfrieze@gmail.com>
2017-09-23 14:32:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def line(array)
+if array == katz_deli
+  puts "The line is currently empty."
+end
+if array == other_deli
+  puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+end
+if array == another_deli
+  puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
+end
+end
+
+def take_a_number(array, string)
+  if array == 0
+    array << string
+    return "Welcome, #{string}. You are number #{array.length} in line."
+  else
+      array << "#{i}. #{string}"
+      return "Welcome, #{string}. You are number #{array.length} in line."
+    end
+end
+
+def now_serving(array)
+  if array == []
+    puts "There is nobody waiting to be served!"
+  else puts "Currently serving #{array[0]}."
+    array.shift
+end
+end
