e1feba439fa2719cf9d95d46dde48e41ef293ff1
Pamela Frieze <pfrieze@gmail.com>
2017-09-23 14:39:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(array, string)
   if array == 0
-    puts array << "string"
+    array << "string"
     return "Welcome, #{string}. You are number #{array.length} in line."
     end
 end
